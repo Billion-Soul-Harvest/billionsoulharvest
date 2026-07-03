2942771fc3e4bff069b272c214b261bf2235e056
Bertwin Romero <bertwinr2@gmail.com>
2026-07-03 09:25:06 +0800

    chore: remove debug env endpoint

--- billionsoulharvest/src/app/api/debug-env/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    ADMIN_DOMAIN: process.env.ADMIN_DOMAIN ?? "NOT SET",
-    NEXT_PUBLIC_ADMIN_DOMAIN: process.env.NEXT_PUBLIC_ADMIN_DOMAIN ?? "NOT SET",
-  });
-}
